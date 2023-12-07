9a14e408073516d7143e2ae72b66f1b30d7e0f58
Shicheng Li <shicheng.li@kcl.ac.uk>
2023-12-07 22:01:58 +0000

    asd

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,11 @@
-"""Forms of the project."""
+from django import forms
+from .models import Thing
 
-# Create your forms here.
+class ThingForm(forms.ModelForm):
+    class Meta:
+        model = Thing
+        fields = ['name', 'description', 'quantity']
+        widgets = {
+            'description': forms.Textarea(attrs={'rows': 4, 'cols': 40}),
+            'quantity': forms.NumberInput()
+        }
